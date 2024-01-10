8ad2aea4aeb1447dddd675821a4d700e1adbe6b4
Sandeep Urankar <sandeepurankar.98@gmail.com>
2024-01-11 00:12:08 +0530

    Added an annotation RequiresApi which was causing error.

--- app/src/main/java/com/sandeepurankar/notes/data/model/Note.kt
+++ app/src/main/java/com/sandeepurankar/notes/data/model/Note.kt
@@ -1,9 +1,11 @@
 package com.sandeepurankar.notes.data.model
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.time.LocalDateTime
 import java.util.UUID
 
-
+@RequiresApi(Build.VERSION_CODES.O)
 data class Note(
     val id: UUID = UUID.randomUUID(),
     val title: String,
